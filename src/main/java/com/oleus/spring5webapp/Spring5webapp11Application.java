e86203f64070326234c5e30b23fb99994eb00cc4
GuillermoOleus <guillermo.hernandez.dev@gmail.com>
2020-05-20 00:04:16 -0500

    Commit

---
+++ src/main/java/com/oleus/spring5webapp/Spring5webapp11Application.java
@@ -0,0 +1,13 @@
+package com.oleus.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webapp11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webapp11Application.class, args);
+	}
+
+}
