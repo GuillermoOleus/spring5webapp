9381a4cf4ad82e6852a55ee1db0a9b11aecbc972
GuillermoOleus <guillermo.hernandez.dev@gmail.com>
2020-05-20 14:05:06 -0500

    author list

---
+++ src/main/java/com/oleus/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package com.oleus.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.oleus.spring5webapp.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+
+	AuthorRepository authorRepository;
+		
+	public AuthorController(AuthorRepository authorRepository) {
+		super();
+		this.authorRepository = authorRepository;
+	}
+
+	@RequestMapping("/authors")
+	public String getAuthors(Model model) {
+		model.addAttribute("authors", authorRepository.findAll());
+		return "authors/list";
+	}
+}
